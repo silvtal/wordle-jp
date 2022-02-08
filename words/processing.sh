075d5ae1945199f19cc97b6d0a3f15fff6180f20
silvtal <stalaveramarcos@gmail.com>
2022-02-08 09:00:43 +0100

    plausible; new 'bad' words; minor change in processing.sh

--- words/processing.sh
+++ words/processing.sh
@@ -8,15 +8,14 @@ grep -x '.\{5\}' < japanese_wordnet_words/romaji-words.txt > wordle-jp-5-raw.txt
 # add missing but suitable words
 cat extra-words.txt >> wordle-jp-5-raw.txt
 # filtering by "are these words ok"
-#             remove loans/inappropiate words...   remove ', spaces...     remove repeats  nums again
-grep -v -x -f bad-words.txt wordle-jp-5-raw.txt | grep -v "'" | grep -v " " | sort -u | grep -v '[0-9]' > jp-words-5.txt 
+#             remove loans/inappropiate words...   remove ', spaces...     　 nums again
+grep -v -x -f bad-words.txt wordle-jp-5-raw.txt | grep -v "'" | grep -v " " | grep -v '[0-9]' > jp-words-5.txt 
+###　DONT remove repeats with sort -u . they are different words and we'll see that when I put in kanji
 
 # filtered by "have we seen them already?"
 cp jp-words-5.txt wordle-jp-5-filtered.txt # in buld-custom-tasks we can see this is "all" words
 cp jp-words-5.txt wordle-jp-5-filter1.txt  # 1 has all suitable words. 2 is the same as 1 but removing the already seen ones, which are marked in filtered with a preceeding dot. in buld-custom-tasks we can see this is "all new" words
+cp jp-words-5.txt jp-words-5-filtered.txt
 
-# TODO filter2 has the used ones. How does that work?? add SHIKU
-
-## correr node getnew
-## importante: correr "node build-custom-tasks.js --prod" para re-generar words.js
-
+## "node ../build-custom-tasks.js --prod"
+## important: run "node words/wordle-jp-getnew.js" to re-generate words.js
